0aae7338e9aa8be5f6a0e50f379c8483c946bf64
Jason Johnson <jasonjohnson47@gmail.com>
2022-01-28 15:27:41 +0000

    making progress

--- solve-maze.js
+++ solve-maze.js
@@ -1,12 +1,12 @@
-const pot = [
+const maze = [
     [0, 0, 1, 0, 1],
     [1, 1, 0, 0, 0],
-    [0, 1, 0, 1, 0], 
-    [0, 0, 0, 1, 0], 
-    [1, 1, 1, 1, 0]
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0],
+    [1, 1, 1, 1, 0],
 ];
 
-let potCopy = pot.map((row) => [...row]);
+let mazeCopy = maze.map((row) => [...row]);
 
 /*let nodesTraveled = [
     {x: 3, y: 0, to: 'down'}, // do we need this first one? to keep track of current entry point being tested?
@@ -21,7 +21,7 @@ let potCopy = pot.map((row) => [...row]);
 */
 // trying next direction...
 /*
-potCopy = [
+mazeCopy = [
     [1, 1, 1, 0, 1],
     [1, 1, 1, 0, 0],
     [1, 1, 1, 1, 0], 
@@ -39,51 +39,140 @@ nodesTraveled = [
 ];
 */
 
-const hasCloggedLevel = pot.some((level) => level.every((space) => space == 1));
+const hasCloggedLevel = maze.some((level) =>
+    level.every((space) => space == 1)
+);
 
 if (hasCloggedLevel) {
     return false;
 }
 
-const entryPoints = potCopy[0].reduce((acc, curr, index) => {
+function isZeroLeft(maze, node) {
+    if (node.x == 0) {
+        return false;
+    }
+    return maze[node.y][node.x - 1] == 0;
+}
+function isZeroRight(maze, node) {
+    if (node.x == maze[node.y].length - 1) {
+        return false;
+    }
+    return maze[node.y][node.x + 1] == 0;
+}
+function isZeroDown(maze, node) {
+    if (node.y == maze.length - 1) {
+        return false;
+    }
+    return maze[node.y + 1][node.x] == 0;
+}
+function isZeroUp(maze, node) {
+    if (node.y == 0) {
+        return false;
+    }
+    return maze[node.y - 1][node.x] == 0;
+}
+
+function getPossibleDirections(maze, node) {
+    const possibleDirections = [];
+
+    if (isZeroDown(maze, node)) {
+        possibleDirections.push('down');
+    }
+    if (isZeroLeft(maze, node)) {
+        possibleDirections.push('left');
+    }
+    if (isZeroRight(maze, node)) {
+        possibleDirections.push('right');
+    }
+    if (isZeroUp(maze, node)) {
+        possibleDirections.push('up');
+    }
+
+    return possibleDirections;
+}
+
+const nodesTraveled = [];
+
+const entryPoints = mazeCopy[0].reduce((acc, curr, index) => {
     if (curr == 0) {
-        acc.push(index);
+        acc.push({ x: index, y: 0 });
     }
     return acc;
 }, []);
 
-function isZeroLeft(pot, node) {
-    if (node.x == 0) {
-        return false;
+entryPoints.forEach(function (entryPoint) {
+    if (isZeroDown(mazeCopy, entryPoint)) {
+        tryPath(mazeCopy, entryPoint);
+    } else {
+        console.log(`cannot move down at ${entryPoint.x}`);
     }
-    return pot[node.y][node.x - 1] == 0;
-}
-function isZeroRight(pot, node) {
-    if (node.x == pot[node.y].length - 1) {
+});
+
+function tryPath(maze, point) {
+    if (nodesTraveled.length > 10) {
         return false;
+    } else {
+        nodesTraveled.push(point);
+        var currentNode = nodesTraveled[nodesTraveled.length - 1];
+        var lastNode = nodesTraveled[nodesTraveled.length - 2];
+        currentNode.try = getPossibleDirections(maze, point);
+
+        if (
+            lastNode &&
+            lastNode.tried &&
+            currentNode.try.includes(lastNode.tried)
+        ) {
+            currentNode.try = currentNode.try.filter(
+                (dir) => !lastNode.tried.includes(dir)
+            );
+        }
+
+        if (currentNode.try[0]) {
+            if (currentNode.tried) {
+                currentNode.tried.push(currentNode.try[0]);
+            } else {
+                currentNode.tried = [currentNode.try[0]];
+            }
+
+            makeMove(maze, point, currentNode.try[0]);
+
+            currentNode.try.shift();
+        } else {
+            console.log('deadend');
+        }
     }
-    return pot[node.y][node.x + 1] == 0;
 }
 
-console.log('isZeroLeft at {x: 0, y: 0}: ', isZeroLeft(potCopy, {x: 0, y: 0}));
-console.log('isZeroLeft at {x: 3, y: 1}: ', isZeroLeft(potCopy, {x: 3, y: 1}));
+function makeMove(maze, point, direction) {
+    var newPoint = { ...point };
 
-console.log('isZeroRight at {x: 0, y: 0}: ', isZeroRight(potCopy, {x: 0, y: 0}));
-console.log('isZeroRight at {x: 3, y: 1}: ', isZeroRight(potCopy, {x: 3, y: 1}));
+    if (direction == 'down') {
+        newPoint.y = newPoint.y + 1;
+    }
+    if (direction == 'left') {
+        newPoint.x = newPoint.x - 1;
+    }
+    if (direction == 'right') {
+        newPoint.x = newPoint.x + 1;
+    }
+    if (direction == 'up') {
+        newPoint.y = newPoint.y - 1;
+    }
 
-function getPossibleDirections() {
+    console.log('newPoint', newPoint);
 
+    tryPath(maze, newPoint);
 }
 
-//entryPoints.forEach();
+console.log(nodesTraveled);
 
 /*
-check to see if any levels have all ones (no zeros), if so, pot cannot drain
-check to see if on last level, if so, pot can drain
+check to see if any levels have all ones (no zeros), if so, maze cannot be completed
+check to see if on last level, if so, maze completed
 
 starting at an entry point on the top level, see if you can move down,
     if not, move on to test the next entry point
-        if no more entry points, pot cannot drain
+        if no more entry points, maze cannot be completed
     if so, move down and add the new node to the list of nodes traveled
 
     add all possible directions to the 'try' property ('down', 'left', 'right', 'up'; do not include the direction you came from)
@@ -93,7 +182,7 @@ starting at an entry point on the top level, see if you can move down,
 
         when you reach a deadend...
         find the last node traveled that has a 'try' array with more directions to try
-        update the potCopy to mark all later nodes as 1s
+        update the mazeCopy to mark all later nodes as 1s
         continue from that last node trying one of the directions from the 'try' property (remove that direction from the 'try' array)
 
 */
